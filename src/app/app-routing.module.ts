24d9374edfd50b79f1568f5394c35866f88bc460
Omkar Killarikar <okmac@Omkars-MacBook-Air.local>
2018-05-30 15:45:03 +0530

    constants remmoved from routing, breaks after build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,14 @@ import {AdminLoginComponent} from '../adminLogin/adminLogin.component';
 import {AdminDashboardComponent} from '../adminDashboard/adminDashboard.component';
 import {UserLoginComponent} from '../userLogin/userLogin.component';
 import {UserDashboardComponent} from '../userDashboard/userDashboard.component';
-import {Constants} from '../utils/constants';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/' + Constants.RoutePath.SELECT_USER, pathMatch: 'full'},
-  {path: Constants.RoutePath.SELECT_USER, component: DashboardComponent},
-  {path: Constants.RoutePath.ADMIN_LOGIN, component: AdminLoginComponent},
-  {path: Constants.RoutePath.ADMIN_DASHBOARD, component: AdminDashboardComponent},
-  {path: Constants.RoutePath.USER_LOGIN, component: UserLoginComponent},
-  {path: Constants.RoutePath.USER_DASHBOARD, component: UserDashboardComponent}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'adminLogin', component: AdminLoginComponent},
+  {path: 'adminDashboard', component: AdminDashboardComponent},
+  {path: 'userLogin', component: UserLoginComponent},
+  {path: 'userDashboard', component: UserDashboardComponent}
 ];
 
 @NgModule({
